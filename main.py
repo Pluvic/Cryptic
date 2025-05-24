ad61982d8c642fa13246e397d500d62d182c6406
Victor Pluvinage <victorpluvinage25@gmail.com>
2025-05-24 15:00:33 +0200

    Add Fromhex command

--- main.py
+++ main.py
@@ -24,16 +24,20 @@ async def on_ready():
     except Exception as e:
         print(f"Failed to sync commands: {e}")
 
-# Define a slash command
-@bot.tree.command(name="hello", description="Prints a hello world message")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello, world!")
-
 # Command that convert a utf8 string to hexadecimal
 @bot.tree.command(name="tohex", description="Convert a utf8 string to hexadecimal")
 async def to_hex(interaction: discord.Interaction, text: str):
     hex_string = text.encode("utf-8").hex()
     await interaction.response.send_message(f"Hexadecimal: {hex_string}")
+
+# Command that convert a hexadecimal string to utf8
+@bot.tree.command(name="fromhex", description="Convert a hexadecimal string to utf8")
+async def from_hex(interaction: discord.Interaction, hex_string: str):
+    try:
+        utf8_string = bytes.fromhex(hex_string).decode("utf-8")
+        await interaction.response.send_message(f"UTF-8 String: {utf8_string}")
+    except ValueError:
+        await interaction.response.send_message("Invalid hexadecimal string.", ephemeral=True)
 
 # Command who decrypt a caesar cipher
 @bot.tree.command(name="caesar", description="Decrypt a caesar cipher")
